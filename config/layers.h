7a7a96228ce395f88f84053a46cd3c6188e22800
6yozo <66yozo@gmail.com>
2023-10-28 20:20:44 +0200

    Remove taipoo, simplify combos

--- config/layers.h
+++ config/layers.h
@@ -27,38 +27,35 @@
 #define WEN_DAL   6
 // Diaresis
 #define WEN_DL    7
-#define WEN_TAIPO 8
 
 #define WEN_LAYER_SET WEN_BASE WEN_NAV WEN_NUM WEN_SYM WEN_AL WEN_DAL WEN_DL
 
 // LAYERS FOR WIN-HUNGARIAN-QUERTZ HOST (WHU)
 
-#define WHU_BASE  9
-#define WHU_NAV   10
-#define WHU_NUM   11
-#define WHU_SYM   12
+#define WHU_BASE  8
+#define WHU_NAV   9
+#define WHU_NUM   10
+#define WHU_SYM   11
 // Acute
-#define WHU_AL    13
+#define WHU_AL    12
 // Double acute
-#define WHU_DAL   14
+#define WHU_DAL   13
 // Diaresis
-#define WHU_DL    15
-#define WHU_TAIPO 16
+#define WHU_DL    14
 
 #define WHU_LAYER_SET WHU_BASE WHU_NAV WHU_NUM WHU_SYM WHU_AL WHU_DAL WHU_DL
 
 // LAYERS FOR MAC-HUNGARIAN-QUERTZ HOST (MHU)
 
-#define MHU_BASE  17
-#define MHU_NAV   18
-#define MHU_NUM   19
-#define MHU_SYM   20
+#define MHU_BASE  15
+#define MHU_NAV   16
+#define MHU_NUM   17
+#define MHU_SYM   18
 // Acute
-#define MHU_AL    21
+#define MHU_AL    19
 // Double acute
-#define MHU_DAL   22
+#define MHU_DAL   20
 // Diaresis
-#define MHU_DL    23
-#define MHU_TAIPO 24
+#define MHU_DL    21
 
 #define MHU_LAYER_SET MHU_BASE MHU_NAV MHU_NUM MHU_SYM MHU_AL MHU_DAL MHU_DL
