1a21575015ee573704705d7413c06f53c3555dc1
6yozo <66yozo@gmail.com>
2023-11-25 16:32:02 +0100

    Add macros for WHU layout

---
+++ config/whu.h
@@ -0,0 +1,63 @@
+// Copyright 2023 Gyozo Gaspar
+//
+// This file is part of Lottie layout. The Lottie layout is free
+// software: you can redistribute it and/or modify it under the terms
+// of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option)
+// any later version.  The Lottie layout is distributed in the hope
+// that it will be useful, but WITHOUT ANY WARRANTY; without even the
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU General Public License for more details.  You
+// should have received a copy of the GNU General Public License along
+// with Lottie layout. If not, see <https://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <behaviors.dtsi>
+//#include <dt-bindings/zmk/keys.h>
+//#include <dt-bindings/zmk/bt.h>
+//#include <dt-bindings/zmk/rgb.h>
+//#include <dt-bindings/zmk/outputs.h>
+//#include <dt-bindings/zmk/ext_power.h>
+
+#include "macros_whu.h"
+
+#define WHU_U_ACUTE     &m_u_acute
+#define WHU_Z           &kp Y
+#define WHU_L           &kp L
+#define WHU_D           &kp D
+#define WHU_P           &kp P
+#define WHU_V           &kp V
+#define WHU_J           &kp J
+#define WHU_K           &kp K
+#define WHU_O           &kp O
+#define WHU_U           &kp U
+#define WHU_DOT         &kp DOT
+#define WHU_I_ACUTE     &m_i_acute
+#define WHU_O_DACUTE    &m_o_dacute
+#define WHU_N           &kp N
+#define WHU_R           &kp R
+#define WHU_T           &kp T
+#define WHU_C           &kp C
+#define WHU_B           &kp B
+#define WHU_Y           &kp Z
+#define WHU_H           &kp H
+#define WHU_A           &kp A
+#define WHU_E           &kp E
+#define WHU_I           &kp I
+#define WHU_O_DIARESIS  &m_o_diaresis
+#define WHU_U_DACUTE    &m_u_dacute
+#define WHU_X           &kp X
+#define WHU_Q           &kp Q
+#define WHU_M           &kp M
+#define WHU_G           &kp G
+#define WHU_W           &kp W
+#define WHU_O_ACUTE     &m_o_acute
+#define WHU_F           &kp F
+#define WHU_E_ACUTE     &m_e_acute
+#define WHU_A_ACUTE     &m_a_acute
+#define WHU_COMMA       &kp COMMA
+#define WHU_U_DIARESIS  &m_u_diaresis
+#define WHU_S           &kp S
+#define WHU_BSPC        &kp BSPC
+#define WHU_SPACE       &kp SPACE
